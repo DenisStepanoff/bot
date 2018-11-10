7b0450711903af1ff384001670a7d212e4e9b018
dstepanov1630 <dstepanov1630@gmail.com>
2018-11-10 23:14:55 +0300

    Added config, proxies, methods:get_update_in_json,last_update.

--- app/bot.py
+++ app/bot.py
@@ -1,11 +1,26 @@
 import requests
 import datetime
+from config import Config
 
 class Tbot:
 	def __init__(self):
-		return print("bot created")
+		return print(Config.URL)
+
+	def get_update_in_json(self, request):
+		response = requests.get(request + "getUpdates", proxies=Config.PROXIES)
+		return response.json()
+
+	def last_update(self, data):
+		results = data['result']
+		total_updates = len(results) - 1
+		return results[total_updates]
+
+
 def main():
 	mybot = Tbot()
+	#print(mybot.get_update_in_json(Config.URL))
+	print(mybot.last_update(mybot.get_update_in_json(Config.URL)))
 
 if __name__ == '__main__':
 	main()
+	
---
+++ app/config.py
@@ -0,0 +1,23 @@
+import os
+
+class Config():
+	#in shell type: export TELTOKEN=past_you_bot_token_here
+	TOKEN = os.environ.get('TELTOKEN')
+	URL = "https://api.telegram.org/bot" + TOKEN + "/"
+	#for proxying requests to the api, you can configure TOR+privoxy:
+	#on ubuntu 16.04:
+	##apt-get install tor privoxy
+	##nano /etc/privoxy/config
+	#add to end of file these three lines:
+	##forward-socks5 / localhost:9050 .
+    ##forward-socks4 / localhost:9050 .
+    ##forward-socks4a / localhost:9050 .
+    #start services:
+    ##service tor start
+    ##service privoxy start
+    #use proxy on localhost:8118
+	PROXIES = {
+        "http": "localhost:8118",
+        "https": "localhost:8118",
+    }
+
