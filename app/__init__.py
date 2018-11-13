eb2b01eb1d9580f5cef90b085cf46d1e189578ba
dstepanov1630 <dstepanov1630@gmail.com>
2018-11-13 22:13:14 +0300

    Added Echo-bot algorithm

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,2 @@
+#the application is packet
 pass
--- app/bot.py
+++ app/bot.py
@@ -1,24 +1,25 @@
 import requests
+from time import sleep
 import datetime
+from time import sleep
 #import configuration from config.py
 from config import Config
 
 class Tbot:
 	def __init__(self):
 		return print(Config.URL)
-    
-    #get all available updates
+	#get all available updates
 	def get_update_in_json(self, request):
 		response = requests.get(request + "getUpdates", proxies=Config.PROXIES)
 		return response.json()
 
-    #get only last update
+	#get only last update
 	def last_update(self, data):
 		results = data['result']
 		total_updates = len(results) - 1
 		return results[total_updates]
-    
-    #get current chat ID
+
+	#get current chat ID
 	def get_chat_id(self, update):
 		return update['message']['chat']['id']
 
@@ -33,9 +34,22 @@ def main():
 	mybot = Tbot()
 	#print(mybot.get_update_in_json(Config.URL))
 	#print(mybot.last_update(mybot.get_update_in_json(Config.URL)))
-	chat_id = mybot.get_chat_id(mybot.last_update(mybot.get_update_in_json(Config.URL)))
-	mybot.send_message(chat_id, 'Bot says: By!')
-	print('MESSAGE is SENDED!')
+	#chat_id = mybot.get_chat_id(mybot.last_update(mybot.get_update_in_json(Config.URL)))
+	#mybot.send_message(chat_id, 'Bot says: Hello!')
+	#print('MESSAGE is SENDED!')
+	update_id = mybot.last_update(mybot.get_update_in_json(Config.URL))['update_id']
+	print('Update_id: {}'.format(update_id))
+
+	while True:
+		if update_id == mybot.last_update(mybot.get_update_in_json(Config.URL))['update_id']:
+			get_update = mybot.get_update_in_json(Config.URL)
+			chat_id = mybot.get_chat_id(mybot.last_update(get_update)) 
+			message = mybot.last_update(get_update)['message']['text']
+			mybot.send_message(chat_id, message)
+			update_id += 1
+		sleep(5)
+
+
 
 if __name__ == '__main__':
 	main()
